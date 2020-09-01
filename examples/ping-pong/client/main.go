4ad4ecf3f1b784728ad2372b6a20055553f6be78
YamiOdymel <yamiodymel@gmail.com>
2020-09-02 03:11:00 +0800

    Ping pong 測試

---
+++ examples/ping-pong/client/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/teacat/maxim"
+)
+
+func main() {
+	log.Println("Running...")
+
+	c, _, err := maxim.NewClient(&maxim.ClientConfig{
+		Address: "ws://localhost:8080/ws",
+	})
+	if err != nil {
+		panic(err)
+	}
+	for {
+		msg, err := c.Read()
+		if err != nil {
+			panic(err)
+		}
+		log.Println("received: " + msg)
+	}
+}
---
+++ examples/ping-pong/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+
+	"time"
+
+	"github.com/teacat/maxim"
+)
+
+func main() {
+	m := maxim.NewDefault()
+	m.HandleConnect(func(s *maxim.Session) {
+		log.Printf("connected: %+v", s)
+		for {
+			<-time.After(time.Second * 1)
+			s.Ping()
+		}
+	})
+
+	log.Println("Running...")
+
+	http.HandleFunc("/ws", m.HandleRequest)
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	err = http.Serve(l, nil)
+	if err != nil {
+		panic(err)
+	}
+}
